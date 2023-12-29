3b46765df86a94e1986cd31b3b8aab533b9b2255
Rahul <rahulpaditkar@gmail.com>
2023-12-29 16:13:45 +0530

    Law_firm

---
+++ src/pages/LawFirm.js
@@ -0,0 +1,413 @@
+import styles from "./LawFirm.module.css";
+
+const LawFirm = () => {
+  return (
+    <div className={styles.lawFirm}>
+      <div className={styles.lawFirmChild} />
+      <div className={styles.groupParent}>
+        <div className={styles.homeParent}>
+          <div className={styles.home}>Home</div>
+          <div className={styles.attorneys}>Attorneys</div>
+          <div className={styles.practiceAreas}>Practice Areas</div>
+          <div className={styles.aboutUs}>About Us</div>
+        </div>
+        <div className={styles.rectangleParent}>
+          <div className={styles.groupChild} />
+          <div className={styles.contactNow}>Contact Now</div>
+        </div>
+        <img className={styles.groupItem} alt="" src="/group-10386@2x.png" />
+        <div className={styles.youDontHaveContainer}>
+          <p className={styles.youDontHave}>{`You don’t have to `}</p>
+          <p className={styles.fightThemAlone}>
+            <b>Fight them Alone.</b>
+          </p>
+        </div>
+        <div className={styles.rectangleGroup}>
+          <div className={styles.groupInner} />
+          <div className={styles.hiconBoldMessage35Parent}>
+            <img
+              className={styles.hiconBoldMessage35}
+              alt=""
+              src="/hicon--bold--message-35@2x.png"
+            />
+            <div className={styles.enterYourEamil}>
+              Enter your eamil address
+            </div>
+          </div>
+          <div className={styles.rectangleContainer}>
+            <div className={styles.rectangleDiv} />
+            <div className={styles.letsTalk}>Let’s Talk</div>
+          </div>
+        </div>
+        <div className={styles.loremIpsumDolor}>
+          Lorem ipsum dolor sit amet consectetur adipiscing elit blandit,
+          curabitur sodales conubia ut inceptos faucibus himenaeos tortor eget,
+          hac massa gravida arcu interdum proin curae.
+        </div>
+      </div>
+      <div className={styles.letsIntroduceOurselfParent}>
+        <div className={styles.letsIntroduceOurselfContainer}>
+          <p className={styles.fightThemAlone}>Let’s Introduce</p>
+          <p className={styles.fightThemAlone}>Ourself</p>
+        </div>
+        <div className={styles.criminalLawyerParent}>
+          <div className={styles.home}>Criminal Lawyer</div>
+          <div className={styles.ametMinimMollitContainer}>
+            <p className={styles.fightThemAlone}>
+              Amet minim mollit non deserunt ullamco est
+            </p>
+            <p className={styles.fightThemAlone}>
+              sit aliqua dolor do amet sint. Velit officia consequatduis
+            </p>
+            <p className={styles.fightThemAlone}>
+              enim velit mollit Exercitation.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.whyChooseUsParent}>
+        <div className={styles.whyChooseUs}>Why Choose us?</div>
+        <div className={styles.groupDiv}>
+          <div className={styles.groupChild1} />
+          <img className={styles.groupIcon} alt="" src="/group-19@2x.png" />
+          <div className={styles.successRate}>98% Success Rate</div>
+          <div className={styles.rectangleParent1}>
+            <div className={styles.groupChild2} />
+            <div className={styles.readMore}>Read More</div>
+          </div>
+          <div className={styles.ametMinimMollitContainer1}>
+            <p className={styles.fightThemAlone}>
+              Amet minim mollit non deserunt ullamco est
+            </p>
+            <p className={styles.fightThemAlone}>
+              sit aliqua dolor do amet sint. Velit officia
+            </p>
+            <p className={styles.fightThemAlone}>
+              consequatduis enim velit mollit Exer.
+            </p>
+          </div>
+        </div>
+        <div className={styles.rectangleParent2}>
+          <div className={styles.groupChild3} />
+          <img className={styles.groupIcon} alt="" src="/group-19@2x.png" />
+          <div className={styles.successRate}>100% Success Rate</div>
+          <div className={styles.rectangleParent1}>
+            <div className={styles.groupChild2} />
+            <div className={styles.readMore}>Read More</div>
+          </div>
+          <div className={styles.ametMinimMollitContainer1}>
+            <p className={styles.fightThemAlone}>
+              Amet minim mollit non deserunt ullamco est
+            </p>
+            <p className={styles.fightThemAlone}>
+              sit aliqua dolor do amet sint. Velit officia
+            </p>
+            <p className={styles.fightThemAlone}>
+              consequatduis enim velit mollit Exer.
+            </p>
+          </div>
+        </div>
+        <div className={styles.rectangleParent4}>
+          <div className={styles.groupChild1} />
+          <img className={styles.groupIcon} alt="" src="/group-19@2x.png" />
+          <div className={styles.successRate}>100% Success Rate</div>
+          <div className={styles.rectangleParent1}>
+            <div className={styles.groupChild2} />
+            <div className={styles.readMore}>Read More</div>
+          </div>
+          <div className={styles.ametMinimMollitContainer1}>
+            <p className={styles.fightThemAlone}>
+              Amet minim mollit non deserunt ullamco est
+            </p>
+            <p className={styles.fightThemAlone}>
+              sit aliqua dolor do amet sint. Velit officia
+            </p>
+            <p className={styles.fightThemAlone}>
+              consequatduis enim velit mollit Exer.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.rectangleParent6}>
+        <img
+          className={styles.rectangleIcon}
+          alt=""
+          src="/rectangle-3651@2x.png"
+        />
+        <img
+          className={styles.groupChild9}
+          alt=""
+          src="/rectangle-3652@2x.png"
+        />
+        <div className={styles.areaOfPractices}>Area of Practices</div>
+        <img
+          className={styles.groupChild10}
+          alt=""
+          src="/rectangle-3655@2x.png"
+        />
+        <img
+          className={styles.groupChild11}
+          alt=""
+          src="/rectangle-3653@2x.png"
+        />
+        <img
+          className={styles.groupChild12}
+          alt=""
+          src="/rectangle-3656@2x.png"
+        />
+        <img
+          className={styles.groupChild13}
+          alt=""
+          src="/rectangle-3654@2x.png"
+        />
+        <div className={styles.businessLaw}>BUSINESS LAW</div>
+        <div className={styles.partnershipLaw}>Partnership LAW</div>
+        <div className={styles.businessLaw1}>BUSINESS LAW</div>
+        <div className={styles.realEstateLawContainer}>
+          <p className={styles.fightThemAlone}>REAL ESTATE LAW</p>
+        </div>
+        <div className={styles.landlordDisputes}>
+          <p className={styles.fightThemAlone}>&nbsp;</p>
+          <p className={styles.fightThemAlone}>LANDLORD DISPUTES</p>
+        </div>
+        <div className={styles.elderAbuse}>ELDER ABUSE</div>
+      </div>
+      <div className={styles.whatSaysOurHappyClientsParent}>
+        <div className={styles.whatSaysOurContainer}>
+          <p className={styles.fightThemAlone}>What says our</p>
+          <p className={styles.fightThemAlone}>happy Clients</p>
+        </div>
+        <img className={styles.groupChild14} alt="" src="/group-42@2x.png" />
+        <div className={styles.rectangleParent7}>
+          <div className={styles.groupChild15} />
+          <img className={styles.ellipseIcon} alt="" src="/ellipse-14@2x.png" />
+          <div className={styles.ametMinimMollitContainer4}>
+            <p className={styles.fightThemAlone}>
+              Amet minim mollit non deserunt ullamco est
+            </p>
+            <p className={styles.fightThemAlone}>
+              sit aliqua dolor do amet sint. Velit officia
+            </p>
+            <p className={styles.fightThemAlone}>
+              consequatduis enim velit mollit Exer. sit
+            </p>
+            <p className={styles.fightThemAlone}>
+              aliqua dolor do amet sint. Velit officia
+            </p>
+          </div>
+          <div className={styles.janeCooper}>Jane Cooper</div>
+          <div className={styles.ceoOfHunt}>Ceo of Hunt</div>
+        </div>
+        <div className={styles.groupChild16} />
+        <img className={styles.groupChild17} alt="" src="/ellipse-14@2x.png" />
+        <div className={styles.ametMinimMollitContainer5}>
+          <p className={styles.fightThemAlone}>
+            Amet minim mollit non deserunt ullamco est
+          </p>
+          <p className={styles.fightThemAlone}>
+            sit aliqua dolor do amet sint. Velit officia
+          </p>
+          <p className={styles.fightThemAlone}>
+            consequatduis enim velit mollit Exer. sit
+          </p>
+          <p className={styles.fightThemAlone}>
+            aliqua dolor do amet sint. Velit officia
+          </p>
+        </div>
+        <div className={styles.devonLane}>Devon Lane</div>
+        <div className={styles.ceoOfHunt1}>Ceo of Hunt</div>
+        <div className={styles.rectangleParent8}>
+          <div className={styles.groupChild15} />
+          <img className={styles.ellipseIcon} alt="" src="/ellipse-14@2x.png" />
+          <div className={styles.ametMinimMollitContainer4}>
+            <p className={styles.fightThemAlone}>
+              Amet minim mollit non deserunt ullamco est
+            </p>
+            <p className={styles.fightThemAlone}>
+              sit aliqua dolor do amet sint. Velit officia
+            </p>
+            <p className={styles.fightThemAlone}>
+              consequatduis enim velit mollit Exer. sit
+            </p>
+            <p className={styles.fightThemAlone}>
+              aliqua dolor do amet sint. Velit officia
+            </p>
+          </div>
+          <div className={styles.janeCooper}>Robert Fox</div>
+          <div className={styles.ceoOfHunt}>Ceo of Hunt</div>
+        </div>
+      </div>
+      <div className={styles.rectangleParent9}>
+        <div className={styles.groupChild20} />
+        <div className={styles.groupContainer}>
+          <div className={styles.groupParent1}>
+            <div className={styles.firstPageParent}>
+              <div className={styles.firstPage}>{`Home `}</div>
+              <div className={styles.secondPage}>Attorneys</div>
+              <div className={styles.thirdPage}>
+                <b>Practice Areas</b>
+                <span>{` `}</span>
+              </div>
+              <div className={styles.fifthPage}>About Us</div>
+            </div>
+            <div className={styles.iconParent}>
+              <img className={styles.icon} alt="" src="/icon@2x.png" />
+              <img className={styles.icon1} alt="" src="/icon@2x.png" />
+              <img className={styles.icon2} alt="" src="/icon@2x.png" />
+              <img className={styles.icon3} alt="" src="/icon@2x.png" />
+            </div>
+          </div>
+          <div className={styles.acmeAllRigParent}>
+            <div className={styles.acmeAllRig}>
+              © 2020 Acme. All right reserved.
+            </div>
+            <div className={styles.privacyPolicy}>Privacy Policy</div>
+            <div className={styles.termsOfService}>Terms of Service</div>
+          </div>
+        </div>
+        <img className={styles.groupChild21} alt="" src="/group-10386@2x.png" />
+      </div>
+      <div className={styles.ourTeamParent}>
+        <div className={styles.ourTeam}>Our Team</div>
+        <div className={styles.ellipseParent}>
+          <img
+            className={styles.groupChild22}
+            alt=""
+            src="/ellipse-15@2x.png"
+          />
+          <div className={styles.danialDefParent}>
+            <div className={styles.danialDef}>Danial Def</div>
+            <div className={styles.cases}>301 Cases</div>
+          </div>
+        </div>
+        <div className={styles.ellipseGroup}>
+          <img
+            className={styles.groupChild22}
+            alt=""
+            src="/ellipse-15@2x.png"
+          />
+          <div className={styles.colleenParent}>
+            <div className={styles.danialDef}>Colleen</div>
+            <div className={styles.cases}>180 Cases</div>
+          </div>
+        </div>
+        <div className={styles.ellipseContainer}>
+          <img
+            className={styles.groupChild22}
+            alt=""
+            src="/ellipse-15@2x.png"
+          />
+          <div className={styles.haldoneParent}>
+            <div className={styles.danialDef}>Haldone</div>
+            <div className={styles.cases}>212 Cases</div>
+          </div>
+        </div>
+        <div className={styles.ellipseParent1}>
+          <img
+            className={styles.groupChild22}
+            alt=""
+            src="/ellipse-15@2x.png"
+          />
+          <div className={styles.cesforilaParent}>
+            <div className={styles.danialDef}>Cesforila</div>
+            <div className={styles.cases}>470 Cases</div>
+          </div>
+        </div>
+        <div className={styles.ellipseParent2}>
+          <img
+            className={styles.groupChild22}
+            alt=""
+            src="/ellipse-15@2x.png"
+          />
+          <div className={styles.nikJeoParent}>
+            <div className={styles.danialDef}>Nik Jeo</div>
+            <div className={styles.cases}>350 Cases</div>
+          </div>
+        </div>
+        <div className={styles.groupChild27} />
+        <div className={styles.ellipseParent3}>
+          <img
+            className={styles.groupChild22}
+            alt=""
+            src="/ellipse-16@2x.png"
+          />
+          <div className={styles.sanfoleParent}>
+            <div className={styles.danialDef}>Sanfole</div>
+            <div className={styles.cases5}>850 Cases</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.faqParent}>
+        <div className={styles.faq}>FAQ</div>
+        <div className={styles.ametMinimMollitContainer7}>
+          <p className={styles.fightThemAlone}>
+            Amet minim mollit non deserunt ullamco est sit
+          </p>
+          <p
+            className={styles.fightThemAlone}
+          >{`aliqua dolor do amet sint. `}</p>
+        </div>
+        <div className={styles.ametMinimMollitNonDeseruntParent}>
+          <div className={styles.ametMinimMollitContainer8}>
+            <p className={styles.fightThemAlone}>
+              Amet minim mollit non deserunt ullamco est sit
+            </p>
+            <p className={styles.fightThemAlone}>
+              aliqua dolor do amet sint. Velit officia consequatduis
+            </p>
+            <p className={styles.fightThemAlone}>
+              enim velit mollit Exer. Amet minim mollit non deserunt
+            </p>
+            <p className={styles.fightThemAlone}>
+              ullamco est sit aliqua dolor do amet sint. Velit officia
+              consequatduis enim velit mollit Exer.
+            </p>
+          </div>
+          <div className={styles.doINeed}>
+            Do I need a personal injury report?
+          </div>
+          <div className={styles.howMuchIs}>How much is my case worth?</div>
+          <div className={styles.whatShouldI}>
+            What should I do right after car accidect
+          </div>
+          <div className={styles.howMuchIs1}>How much is my case worth?</div>
+          <div className={styles.ellipseParent4}>
+            <div className={styles.ellipseDiv} />
+            <div className={styles.div}>+</div>
+          </div>
+          <div className={styles.ellipseParent5}>
+            <div className={styles.ellipseDiv} />
+            <div className={styles.div}>+</div>
+          </div>
+          <div className={styles.ellipseParent6}>
+            <div className={styles.ellipseDiv} />
+            <div className={styles.div}>+</div>
+          </div>
+          <div className={styles.lineDiv} />
+          <div className={styles.groupChild31} />
+          <div className={styles.groupChild32} />
+        </div>
+      </div>
+      <div className={styles.rectangleParent10}>
+        <div className={styles.groupChild33} />
+        <div className={styles.subscribeOurNewsletterParent}>
+          <div className={styles.subscribeOurNewsletter}>
+            Subscribe Our Newsletter
+          </div>
+          <div className={styles.rectangleParent11}>
+            <div className={styles.groupChild34} />
+            <div className={styles.groupChild35} />
+            <div className={styles.groupChild36} />
+            <i className={styles.name}>{`Name : `}</i>
+            <i className={styles.enterYourEmail}>Enter your Email</i>
+            <b className={styles.send}>SEND</b>
+          </div>
+        </div>
+      </div>
+      <img className={styles.lawFirmItem} alt="" src="/group-10591@2x.png" />
+      <div className={styles.lawFirmInner} />
+    </div>
+  );
+};
+
+export default LawFirm;
